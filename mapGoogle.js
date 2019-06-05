840060cec919cca14dba1f4f104b4275fe48805f
Francois Grandsire <grandsiref@gmail.com>
2019-06-05 09:32:53 +0200

    presque fini

--- mapGoogle.js
+++ mapGoogle.js
@@ -7,22 +7,38 @@ function initMap() {
     mapTypeId: 'terrain'
   });
 
-
+  
   var script = document.createElement('script');
 
   
   document.getElementsByTagName('head')[0].appendChild(script);
+  
 }
 
 
-function placedMarq(coordun,coorddeux){
- 
+function placedMarq(coordun,coorddeux,utilisateur){
+ console.log(utilisateur);
+  console.log(coorddeux);
+  console.log(coordun);
+  if(utilisateur==1){
+    var marker = new google.maps.Marker({
+    position: {lat:coordun, lng: coorddeux },
+    icon: {path: google.maps.SymbolPath.CIRCLE,scale : 10},
+    map: map});
+  }
+  if(utilisateur==0){
    var marker = new google.maps.Marker({
     position: {lat:coordun, lng: coorddeux },
+     icon: {path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+            scale: 5,
+           strokeColor : "red"},
     map: map});
-  
-  //console.log(event.detail.coordgps[0]);
-  //console.log(myLatlng);
+  }
+
+  console.log(marker);
+  /*var mypoints=new google.maps.LatLng(coordun, coorddeux);
+  console.log(mypoints);
+  bounds.extend(mypoints);*/
   
 }
 
